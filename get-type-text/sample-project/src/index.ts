e60be4fa15e707b16782b01226dc8e69c76b5be6
Sebastian Gurin <sebastigurin@gmail.com>
2019-03-18 00:23:33 -0300

    sp

--- get-type-text/sample-project/src/index.ts
+++ get-type-text/sample-project/src/index.ts
@@ -1,2 +1,5 @@
-
-// const typeName = 
+import TypeText from 'get-type-text'
+type Type<T> = {a: string, b: T}[]
+const n = TypeText<Type<Date>>()
+const b = TypeText<{a:'a'}>()
+const c = TypeText<{a:"a"}>()
